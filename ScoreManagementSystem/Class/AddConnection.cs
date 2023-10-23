1166d42eb56957321f4bf22cbf7857b6d1f94a52
Navan <soknavan2@gmail.com>
2023-10-23 15:48:24 +0700

    Create Project

---
+++ ScoreManagementSystem/Class/AddConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreManagementSystem.Class
+{
+     class AddConnection
+    {
+        public static string GetConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["ScoreManagementSystem"].ConnectionString;
+
+        }
+    }
+}
